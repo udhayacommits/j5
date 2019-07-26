92df8d94ae655bf8eede2e41f71ecf0ef194c203
Kier Davis <kierdavis@gmail.com>
2019-07-26 19:06:39 +0100

    nix environment: make manylinux1 libraries available
    This should make packages in the manylinux1 wheel format work once they are installed.
    
    However, Nix's Python overrides pip's checks for whether the system supports manylinux1
    using the _manylinux.manylinux1_compatible flag (more information about this available at
    https://github.com/NixOS/nixpkgs/issues/18484). This override still needs to be removed before
    manylinux1 packages can be installed in the development environent.

--- shell.nix
+++ shell.nix
@@ -5,7 +5,47 @@
 
 with pkgs;
 
-stdenv.mkDerivation {
+let
+  # Some of our dependencies are distributed as manylinux1 wheels, but Nix's
+  # standard Python environment is not compatible with manylinux1
+  # (https://github.com/NixOS/nixpkgs/issues/18484).
+  # We can work around this by ensuring the shared libraries that must be
+  # provided by the host system as part of the manylinux1 protocol (listed in
+  # PEP 513) are made available on LD_LIBRARY_PATH.
+  # This is based on the work done in https://github.com/NixOS/nixpkgs/pull/55812.
+  manylinux1_lib_map = {
+    "libpanelw.so.5" = ncurses5;
+    "libncursesw.so.5" = ncurses5;
+    "libgcc_s.so.1" = glibc;
+    "libstdc++.so.6" = gcc7.cc;
+    "libm.so.6" = glibc;
+    "libdl.so.2" = glibc;
+    "librt.so.1" = glibc;
+    "libcrypt.so.1" = glibc;
+    "libc.so.6" = glibc;
+    "libnsl.so.1" = glibc;
+    "libutil.so.1" = glibc;
+    "libpthread.so.0" = glibc;
+    "libresolv.so.2" = glibc;
+    "libX11.so.6" = xorg.libX11;
+    "libXext.so.6" = xorg.libXext;
+    "libXrender.so.1" = xorg.libXrender;
+    "libICE.so.6" = xorg.libICE;
+    "libSM.so.6" = xorg.libSM;
+    "libGL.so.1" = libGL;
+    "libgobject-2.0.so.0" = glib;
+    "libgthread-2.0.so.0" = glib;
+    "libglib-2.0.so.0" = glib;
+  };
+  manylinux1_libs = runCommand "manylinux1-libs" {} ''
+    mkdir -p $out/lib
+    ${lib.concatStringsSep "\n"
+      (lib.mapAttrsToList
+        (name: pkg: "ln -s ${lib.getOutput "lib" pkg}/lib/${name} $out/lib/${name}")
+        manylinux1_lib_map)}
+  '';
+
+in stdenv.mkDerivation {
   name = "j5-dev-env";
   buildInputs = [
     gnumake
@@ -13,5 +53,5 @@ stdenv.mkDerivation {
     python3
     python3Packages.poetry
   ];
-  LD_LIBRARY_PATH = [ "${libusb1}/lib" ];
+  LD_LIBRARY_PATH = lib.makeLibraryPath [ manylinux1_libs libusb1 ];
 }
